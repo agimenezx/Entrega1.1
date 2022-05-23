7d8015fc4f88392dbb754008b7687880a2e2db06
unknown <formacio@PC-1062629.formacio.local>
2022-05-23 12:29:59 +0200

    Exercici 1 i 2

---
+++ index.js
@@ -0,0 +1,36 @@
+/* Nivell 1
+- Exercici 1
+Crea una funció que mostri per consola el nom d'usuari al invocar-la passant-li el nom com a paràmetre.
+ */
+
+function userName1(name) {
+
+    console.log(name)
+
+}
+userName1("albert")
+
+/* Nivell 2
+- Exercici 1
+Mostra per consola el nom i cognoms de l'usuari mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge. */
+
+function userName2() {
+    const name = "Albert"
+    const surName = "Giménez"
+
+    console.log(`Nom: ${name} Cognoms: ${surName}`)
+
+}
+
+userName2()
+
+/* - Exercici 2
+Invoca una funció que retorni un valor des de dins d'una template literal. */
+
+
+
+
+/* Nivell 3
+- Exercici 1
+Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola.
+ Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades. */
