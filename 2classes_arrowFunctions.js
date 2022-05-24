adef5d806ebeb4c41331aac78d68f7f97b3b3e44
agimenezx <agimenezx@uoc.edu>
2022-05-25 00:14:01 +0200

    Exercici1 sprint2

---
+++ 2classes_arrowFunctions.js
@@ -0,0 +1,4 @@
+/* - Exercici 1
+Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
+
+((a,b) => console.log(a + b))(5,5)//no està bé tot i que dona el resultat
