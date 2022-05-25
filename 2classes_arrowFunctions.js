53296f37725b7ccdbd44a39f7478361775273a66
albert <agimenezx@uoc.edu>
2022-05-25 09:22:30 +0200

    Nivell 1 Exercici 1 entrega 1.2

--- 2classes_arrowFunctions.js
+++ 2classes_arrowFunctions.js
@@ -1,4 +1,25 @@
-/* - Exercici 1
+
+/* Nivell 1
+ - Exercici 1
 Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
 
-((a,b) => console.log(a + b))(5,5)//no està bé tot i que dona el resultat
+
+/* console.log(
+    ((a, b) => {
+      return a + b;
+    })(5, 5)
+  ); *///Una manera d´apuntar-ho
+
+console.log(((a,b) => (a + b))(5,5))//En una linia no cal return, està implicit
+
+/* Nivell 2
+- Exercici 1
+Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut. */
+
+
+
+
+
+
+
+
